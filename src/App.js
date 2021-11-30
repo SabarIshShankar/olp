f09b6dfb9927c93a3ab926105e8a1cf4a8915951
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-11-30 12:13:35 +0530

    i

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Navbar from "./components/Navbar";
 import Bg from "./components/Banner";
+import Card from "./components/CardBanner";
 export default function App() {
   return (
     <div className="App">
       <Navbar />
       <Bg />
+      <Card />
     </div>
   );
 }
---
+++ src/components/CardBanner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="w3-row">
+      <img src="https://unsplash.com/random" alt="" />
+      <h1>see what you can do with oyela</h1>
+    </div>
+  );
+};
+
+export default Card;
